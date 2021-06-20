4a2caecbf8e9411d0ed78f1ca5dbf27ec4094113
Vladimir Kuleshov <kyleshovbb@gmail.com>
2021-06-20 14:44:45 +0300

    refactor: update connection to database

--- src/db.ts
+++ src/db.ts
@@ -4,24 +4,25 @@ import config from './common/config';
 import UserEntity from './resources/users/user.entity';
 import TaskEntity from './resources/tasks/task.entity';
 import BoardEntity from './resources/boards/board.entity';
+import { logger, parseErrorToLog } from './common/logger';
+import { LogTypes } from './types/logger.types';
 
-export function createDBConnection(cb: Function) {
-  createConnection({
-    type: config.DB_TYPE as 'postgres',
-    host: config.DB_HOST,
-    port: config.DB_PORT,
-    username: config.POSTGRES_USER,
-    password: config.POSTGRES_PASSWORD,
-    database: config.POSTGRES_DB,
-    logging: true,
-    synchronize: true,
-    entities: [UserEntity, TaskEntity, BoardEntity],
-  })
-    .then(() => {
-      cb();
-    })
-    .catch((error) => {
-      // TODO: use logger instead of console.error
-      console.error(error);
+export async function createDBConnection(cb: Function) {
+  try {
+    await createConnection({
+      type: config.DB_TYPE as 'postgres',
+      host: config.DB_HOST,
+      port: config.DB_PORT,
+      database: config.POSTGRES_DB,
+      username: config.POSTGRES_USER,
+      password: config.POSTGRES_PASSWORD,
+      logging: true,
+      synchronize: true,
+      entities: [UserEntity, TaskEntity, BoardEntity],
+      cli: { migrationsDir: 'src/migrations' },
     });
+    cb();
+  } catch (error) {
+    logger.error(parseErrorToLog(error, LogTypes.DBConnectionError));
+  }
 }
--- src/types/logger.types.ts
+++ src/types/logger.types.ts
@@ -3,4 +3,5 @@ export enum LogTypes {
   httpSuccess = '[HTTP_SUCCESS_LOG]',
   uncaughtExceptionError = '[UNCAUGHT_EXCEPTION_ERROR]',
   unhandledRejectionError = '[UNCAUGHT_REJECTION_ERROR]',
+  DBConnectionError = '[DATABASE_CONNECTION_ERROR]',
 }
