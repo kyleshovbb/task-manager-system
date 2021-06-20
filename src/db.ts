29790c9f22b8f3502b62af0bc0b408965fb1e27f
Vladimir Kuleshov <kyleshovbb@gmail.com>
2021-06-20 23:22:59 +0300

    refactor: update database password

--- src/db.ts
+++ src/db.ts
@@ -7,22 +7,23 @@ import BoardEntity from './resources/boards/board.entity';
 import { logger, parseErrorToLog } from './common/logger';
 import { LogTypes } from './types/logger.types';
 
-export async function createDBConnection(cb: Function) {
-  try {
-    await createConnection({
-      type: config.DB_TYPE as 'postgres',
-      host: config.DB_HOST,
-      port: config.DB_PORT,
-      database: config.POSTGRES_DB,
-      username: config.POSTGRES_USER,
-      password: config.POSTGRES_PASSWORD,
-      logging: true,
-      synchronize: true,
-      entities: [UserEntity, TaskEntity, BoardEntity],
-      cli: { migrationsDir: 'src/migrations' },
+export function createDBConnection(cb: Function) {
+  createConnection({
+    type: config.DB_TYPE as 'postgres',
+    host: config.DB_HOST,
+    port: config.DB_PORT,
+    database: config.POSTGRES_DB,
+    username: config.POSTGRES_USER,
+    password: config.POSTGRES_PASSWORD,
+    logging: true,
+    synchronize: true,
+    entities: [UserEntity, TaskEntity, BoardEntity],
+    cli: { migrationsDir: 'src/migrations' },
+  })
+    .then(() => {
+      cb();
+    })
+    .catch((error) => {
+      logger.error(parseErrorToLog(error, LogTypes.DBConnectionError));
     });
-    cb();
-  } catch (error) {
-    logger.error(parseErrorToLog(error, LogTypes.DBConnectionError));
-  }
 }
